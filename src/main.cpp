a0b4fc71e9041ded57a69120fb4a124e90baff65
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-28 20:26:33 +0200

    add the Common submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include "utils/ntsh_core_defines.h"
+#include "../external/Common/ntsh_engine_defines.h"
 
 int main()
 {
+	int a;
+	NTSH_UNUSED(a);
+	NTSH_ASSERT(false);
 	NTSH_CORE_ERROR("NutshellEngine", 1);
 }
