5eec9549bc168aedafeb9b95cd6e230453604796
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-28 20:45:06 +0200

    add enums to the main test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "utils/ntsh_core_defines.h"
 #include "../external/Common/ntsh_engine_defines.h"
+#include "../external/Common/ntsh_engine_enums.h"
 
 int main()
 {
 	int a;
 	NTSH_UNUSED(a);
 	NTSH_ASSERT(false);
-	NTSH_CORE_ERROR("NutshellEngine", 1);
+	NTSH_CORE_ERROR("NutshellEngine", NTSH_RESULT_UNKNOWN_ERROR);
 }
