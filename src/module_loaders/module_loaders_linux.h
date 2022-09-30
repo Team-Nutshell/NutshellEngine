a24711c3504ef05e7fa6fee006400a6e48a886bf
ntsh-oni <teamnutshell.dev@gmail.com>
2022-09-30 19:47:08 +0200

    integrate different types of modules

--- src/module_loaders/module_loaders_linux.h
+++ src/module_loaders/module_loaders_linux.h
@@ -1,14 +1,27 @@
 #pragma once
-#include "../../external/Common/ntsh_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_graphics_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_physics_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_window_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_audio_module_interface.h"
 #include <dlfcn.h>
 #include <string>
-#include <unordered_map>
+#include <utility>
 
 class ModuleLoader {
 public:
-	NutshellModuleInterface* loadModule(const std::string& modulePath);
-	void unloadModule(NutshellModuleInterface* module);
+	NutshellGraphicsModuleInterface* loadGraphicsModule();
+	NutshellPhysicsModuleInterface* loadPhysicsModule();
+	NutshellWindowModuleInterface* loadWindowModule();
+	NutshellAudioModuleInterface* loadAudioModule();
+	void unloadGraphicsModule();
+	void unloadPhysicsModule();
+	void unloadWindowModule();
+	void unloadAudioModule();
 
 private:
-	std::unordered_map<NutshellModuleInterface*, void*> m_modules;
+	std::pair<NutshellGraphicsModuleInterface*, void*> m_graphicsModule;
+	std::pair<NutshellPhysicsModuleInterface*, void*> m_physicsModule;
+	std::pair<NutshellWindowModuleInterface*, void*> m_windowModule;
+	std::pair<NutshellAudioModuleInterface*, void*> m_audioModule;
 };
--- src/module_loaders/module_loaders_windows.h
+++ src/module_loaders/module_loaders_windows.h
@@ -1,14 +1,27 @@
 #pragma once
-#include "../../external/Common/ntsh_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_graphics_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_physics_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_window_module_interface.h"
+#include "../../external/Common/module_interfaces/ntsh_audio_module_interface.h"
 #include <windows.h>
 #include <string>
-#include <unordered_map>
+#include <utility>
 
 class ModuleLoader {
 public:
-	NutshellModuleInterface* loadModule(const std::string& modulePath);
-	void unloadModule(NutshellModuleInterface* module);
+	NutshellGraphicsModuleInterface* loadGraphicsModule();
+	NutshellPhysicsModuleInterface* loadPhysicsModule();
+	NutshellWindowModuleInterface* loadWindowModule();
+	NutshellAudioModuleInterface* loadAudioModule();
+	void unloadGraphicsModule();
+	void unloadPhysicsModule();
+	void unloadWindowModule();
+	void unloadAudioModule();
 
 private:
-	std::unordered_map<NutshellModuleInterface*, HINSTANCE> m_modules;
+	std::pair<NutshellGraphicsModuleInterface*, HINSTANCE> m_graphicsModule;
+	std::pair<NutshellPhysicsModuleInterface*, HINSTANCE> m_physicsModule;
+	std::pair<NutshellWindowModuleInterface*, HINSTANCE> m_windowModule;
+	std::pair<NutshellAudioModuleInterface*, HINSTANCE> m_audioModule;
 };
