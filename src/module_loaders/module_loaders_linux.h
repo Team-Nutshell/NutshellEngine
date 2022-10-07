699f02cc8420ef0dbfd97870ad36a066e50a9966
ntsh-oni <teamnutshell.dev@gmail.com>
2022-10-07 16:34:22 +0200

    cleaner module loading and unloading

--- src/module_loaders/module_loaders_linux.h
+++ src/module_loaders/module_loaders_linux.h
@@ -4,24 +4,57 @@
 #include "../../external/Common/module_interfaces/ntsh_physics_module_interface.h"
 #include "../../external/Common/module_interfaces/ntsh_window_module_interface.h"
 #include "../../external/Common/module_interfaces/ntsh_audio_module_interface.h"
+#include "../utils/ntsh_core_defines.h"
 #include <dlfcn.h>
 #include <string>
-#include <utility>
+#include <map>
+#include <type_traits>
+
+typedef void* createModule_t();
+typedef void destroyModule_t(void*);
 
 class ModuleLoader {
 public:
-	NutshellGraphicsModuleInterface* loadGraphicsModule();
-	NutshellPhysicsModuleInterface* loadPhysicsModule();
-	NutshellWindowModuleInterface* loadWindowModule();
-	NutshellAudioModuleInterface* loadAudioModule();
-	void unloadGraphicsModule();
-	void unloadPhysicsModule();
-	void unloadWindowModule();
-	void unloadAudioModule();
+	template <typename T>
+	T* loadModule(const std::string& modulePath) {
+		NTSH_CORE_INFO("Loading module from: \"" + modulePath + "\".");
+
+		void* moduleLibrary = dlopen(modulePath.c_str(), RTLD_LAZY);
+		if (!moduleLibrary)
+		{
+			NTSH_CORE_ERROR("Could not load the dynamic library.", NTSH_RESULT_MODULE_LIBRARY_LOAD_ERROR);
+		}
+
+		createModule_t* createModule = (createModule_t*)dlsym(moduleLibrary, "createModule");
+		const char* dlsymError = dlerror();
+		if (!createModule) {
+			NTSH_CORE_ERROR("Could not load symbol \"createModule\" from dynamic library: " + std::string(dlsymError), NTSH_RESULT_MODULE_SYMBOL_LOAD_ERROR);
+		}
+
+		T* module = static_cast<T*>(createModule());
+
+		m_modules[typeid(T).name()] = moduleLibrary;
+
+		return module;
+	}
+
+	template <typename T>
+	void unloadModule(T*& module) {
+		NTSH_CORE_INFO("Unloading module \"" + module->getName() + "\".");
+
+		destroyModule_t* destroyModule = (destroyModule_t*)dlsym(m_modules[typeid(T).name()], "destroyModule");
+		const char* dlsymError = dlerror();
+		if (dlsymError) {
+			NTSH_CORE_ERROR("Could not load symbol \"destroyModule\": " + std::string(dlsymError), NTSH_RESULT_MODULE_SYMBOL_LOAD_ERROR);
+		}
+
+		destroyModule(module);
+
+		if (dlclose(m_modules[typeid(T).name()]) != 0) {
+			NTSH_CORE_ERROR("Could not unload the dynamic library: " + std::string(dlsymError), NTSH_RESULT_MODULE_LIBRARY_LOAD_ERROR);
+		}
+	}
 
 private:
-	std::pair<NutshellGraphicsModuleInterface*, void*> m_graphicsModule;
-	std::pair<NutshellPhysicsModuleInterface*, void*> m_physicsModule;
-	std::pair<NutshellWindowModuleInterface*, void*> m_windowModule;
-	std::pair<NutshellAudioModuleInterface*, void*> m_audioModule;
+	std::map<std::string, void*> m_modules;
 };
--- src/module_loaders/module_loaders_windows.h
+++ src/module_loaders/module_loaders_windows.h
@@ -4,24 +4,55 @@
 #include "../../external/Common/module_interfaces/ntsh_physics_module_interface.h"
 #include "../../external/Common/module_interfaces/ntsh_window_module_interface.h"
 #include "../../external/Common/module_interfaces/ntsh_audio_module_interface.h"
+#include "../utils/ntsh_core_defines.h"
 #include <windows.h>
 #include <string>
-#include <utility>
+#include <map>
+#include <type_traits>
+
+typedef void* (__stdcall* createModule_t)();
+typedef void (__stdcall* destroyModule_t)(void*);
 
 class ModuleLoader {
 public:
-	NutshellGraphicsModuleInterface* loadGraphicsModule();
-	NutshellPhysicsModuleInterface* loadPhysicsModule();
-	NutshellWindowModuleInterface* loadWindowModule();
-	NutshellAudioModuleInterface* loadAudioModule();
-	void unloadGraphicsModule();
-	void unloadPhysicsModule();
-	void unloadWindowModule();
-	void unloadAudioModule();
+	template <typename T>
+	T* loadModule(const std::string& modulePath) {
+		NTSH_CORE_INFO("Loading module from: " + modulePath + ".");
+
+		HINSTANCE moduleLibrary = LoadLibrary(modulePath.c_str());
+		if (!moduleLibrary)
+		{
+			NTSH_CORE_ERROR("Could not load the dynamic library.", NTSH_RESULT_MODULE_LIBRARY_LOAD_ERROR);
+		}
+
+		createModule_t createModule = (createModule_t)GetProcAddress(moduleLibrary, "createModule");
+		if (!createModule) {
+			NTSH_CORE_ERROR("Could not load symbol createModule from dynamic library.", NTSH_RESULT_MODULE_SYMBOL_LOAD_ERROR);
+		}
+
+		T* module = static_cast<T*>(createModule());
+
+		m_modules[typeid(T).name()] = moduleLibrary;
+
+		return module;
+	}
+
+	template <typename T>
+	void unloadModule(T*& module) {
+		NTSH_CORE_INFO("Unloading module " + module->getName() + ".");
+
+		destroyModule_t destroyModule = (destroyModule_t)GetProcAddress(m_modules[typeid(T).name()], "destroyModule");
+		if (!destroyModule) {
+			NTSH_CORE_ERROR("Could not load symbol destroyModule.", NTSH_RESULT_MODULE_SYMBOL_LOAD_ERROR);
+		}
+
+		destroyModule(module);
+
+		if (!FreeLibrary(m_modules[typeid(T).name()])) {
+			NTSH_CORE_ERROR("Could not unload the dynamic library.", NTSH_RESULT_MODULE_LIBRARY_LOAD_ERROR);
+		}
+	}
 
 private:
-	std::pair<NutshellGraphicsModuleInterface*, HINSTANCE> m_graphicsModule;
-	std::pair<NutshellPhysicsModuleInterface*, HINSTANCE> m_physicsModule;
-	std::pair<NutshellWindowModuleInterface*, HINSTANCE> m_windowModule;
-	std::pair<NutshellAudioModuleInterface*, HINSTANCE> m_audioModule;
+	std::map<std::string, HINSTANCE> m_modules;
 };
