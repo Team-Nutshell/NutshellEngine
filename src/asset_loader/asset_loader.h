5c0cb30a845390152be158c835574e20f8f2677a
ntsh-oni <teamnutshell.dev@gmail.com>
2022-11-07 13:52:28 +0100

    add resource unloading

--- src/asset_loader/asset_loader.h
+++ src/asset_loader/asset_loader.h
@@ -2,13 +2,20 @@
 #include "../../external/Common/resources/ntsh_resources_audio.h"
 #include "../../external/Common/resources/ntsh_resources_graphics.h"
 #include <string>
+#include <forward_list>
 
 class AssetLoader {
 public:
-	NtshAudio loadAudio(const std::string& filePath);
-	NtshModel loadModel(const std::string& filePath);
+	NtshAudio* loadAudio(const std::string& filePath);
+	NtshModel* loadModel(const std::string& filePath);
+
+	void unloadAudio(NtshAudio* audio);
+	void unloadModel(NtshModel* model);
 
 private:
 	void loadAudioWav(const std::string& filePath, NtshAudio& audio);
 	void loadModelObj(const std::string& filePath, NtshModel& model);
+
+	std::forward_list<NtshAudio> m_audioResources;
+	std::forward_list<NtshModel> m_modelResources;
 };
