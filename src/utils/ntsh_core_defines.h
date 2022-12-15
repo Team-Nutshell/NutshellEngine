a0968a4eccd31fde1c94678594b0e3eeb4a71dfc
ntsh-oni <teamnutshell.dev@gmail.com>
2022-12-15 14:58:58 +0100

    don't use ifdef anymore

--- src/utils/ntsh_core_defines.h
+++ src/utils/ntsh_core_defines.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <iostream>
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_CORE_INFO(message) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
@@ -12,7 +12,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_CORE_WARNING(message) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
@@ -23,7 +23,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_CORE_ERROR(message, code) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
@@ -36,7 +36,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_ASSET_LOADER_INFO(message) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
@@ -47,7 +47,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_ASSET_LOADER_WARNING(message) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
@@ -58,7 +58,7 @@
 	} while(0)
 #endif
 
-#ifdef NTSH_DEBUG
+#if defined(NTSH_DEBUG)
 #define NTSH_ASSET_LOADER_ERROR(message, code) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
