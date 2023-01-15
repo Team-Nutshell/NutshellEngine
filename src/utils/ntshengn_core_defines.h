c337f5c2c75efc85a6105f881ddd40b41bd71dcb
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-15 23:56:17 +0100

    NtshEngn refactoring

--- src/utils/ntshengn_core_defines.h
+++ src/utils/ntshengn_core_defines.h
@@ -1,71 +1,71 @@
 #pragma once
 #include <iostream>
 
-#if defined(NTSH_DEBUG)
-#define NTSH_CORE_INFO(message) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_CORE_INFO(message) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
 	} while(0)
 #else
-#define NTSH_CORE_INFO(message) \
+#define NTSHENGN_CORE_INFO(message) \
 	do { \
 	} while(0)
 #endif
 
-#if defined(NTSH_DEBUG)
-#define NTSH_CORE_WARNING(message) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_CORE_WARNING(message) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
 	} while(0)
 #else
-#define NTSH_CORE_WARNING(message) \
+#define NTSHENGN_CORE_WARNING(message) \
 	do { \
 	} while(0)
 #endif
 
-#if defined(NTSH_DEBUG)
-#define NTSH_CORE_ERROR(message, code) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_CORE_ERROR(message, code) \
 	do { \
 		std::cerr << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
 		exit(1); \
 	} while(0)
 #else
-#define NTSH_CORE_ERROR(message, code) \
+#define NTSHENGN_CORE_ERROR(message, code) \
 	do { \
 		exit(1); \
 	} while(0)
 #endif
 
-#if defined(NTSH_DEBUG)
-#define NTSH_ASSET_LOADER_INFO(message) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_ASSET_LOADER_INFO(message) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
 	} while(0)
 #else
-#define NTSH_ASSET_LOADER_INFO(message) \
+#define NTSHENGN_ASSET_LOADER_INFO(message) \
 	do { \
 	} while(0)
 #endif
 
-#if defined(NTSH_DEBUG)
-#define NTSH_ASSET_LOADER_WARNING(message) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_ASSET_LOADER_WARNING(message) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
 	} while(0)
 #else
-#define NTSH_ASSET_LOADER_WARNING(message) \
+#define NTSHENGN_ASSET_LOADER_WARNING(message) \
 	do { \
 	} while(0)
 #endif
 
-#if defined(NTSH_DEBUG)
-#define NTSH_ASSET_LOADER_ERROR(message, code) \
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_ASSET_LOADER_ERROR(message, code) \
 	do { \
 		std::cerr << "\33[1m\33[94mASSET LOADER \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
 		exit(1); \
 	} while(0)
 #else
-#define NTSH_ASSET_LOADER_ERROR(message, code) \
+#define NTSHENGN_ASSET_LOADER_ERROR(message, code) \
 	do { \
 		exit(1); \
 	} while(0)
