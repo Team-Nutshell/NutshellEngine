aaffcd0646dbb26ab1a8c0f85b9627606c6dc211
ntsh-oni <teamnutshell.dev@gmail.com>
2023-01-16 19:13:03 +0100

    pass modules and ECS to scripts

--- src/scripting/ntshengn_scripting.cpp
+++ src/scripting/ntshengn_scripting.cpp
@@ -8,12 +8,21 @@ void NtshEngn::Scripting::update(double dt) {
 	for (Entity entity : m_entities) {
 		const Scriptable& entityScript = m_ecs->getComponent<Scriptable>(entity);
 
+		entityScript.script->setModules(m_graphicsModule, m_physicsModule, m_windowModule, m_audioModule);
+		entityScript.script->setECS(m_ecs);
 		entityScript.script->update(dt);
 	}
 }
 
 void NtshEngn::Scripting::destroy() {
 
+}
+
+void NtshEngn::Scripting::setModules(GraphicsModuleInterface* graphicsModule, PhysicsModuleInterface* physicsModule, WindowModuleInterface* windowModule, AudioModuleInterface* audioModule) {
+	m_graphicsModule = graphicsModule;
+	m_physicsModule = physicsModule;
+	m_windowModule = windowModule;
+	m_audioModule = audioModule;
 }
 
 void NtshEngn::Scripting::setECS(ECS* ecs) {
