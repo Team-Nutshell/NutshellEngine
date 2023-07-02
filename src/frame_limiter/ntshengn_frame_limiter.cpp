e386bfcc99dd76f841b7f4af072c760f53dd60c6
ntsh-oni <teamnutshell.dev@gmail.com>
2023-07-02 14:27:44 +0200

    split frame limiter in header and cpp files

---
+++ src/frame_limiter/ntshengn_frame_limiter.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include "ntshengn_frame_limiter.h"
+
+void FrameLimiter::wait(double frameStart) {
+	if (m_maxFPS != 0) {
+		double currentTime = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
+		double maxFPSToMilliseconds = 1000.0 / static_cast<double>(m_maxFPS);
+		if ((currentTime - frameStart) < maxFPSToMilliseconds) {
+			double busyWaitingStart = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
+			while ((std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count() - busyWaitingStart) < (maxFPSToMilliseconds - (currentTime - frameStart)));
+		}
+	}
+}
+
+void FrameLimiter::setMaxFPS(uint32_t maxFPS) {
+	m_maxFPS = maxFPS;
+}
+
+uint32_t FrameLimiter::getMaxFPS() {
+	return m_maxFPS;
+}
