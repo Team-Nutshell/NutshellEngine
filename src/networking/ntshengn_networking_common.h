dff8c31ff02823f8e504770f2f8ff907f14d745b
ntsh-oni <teamnutshell.dev@gmail.com>
2023-08-01 12:24:58 +0200

    move networking defines to networking common

--- src/networking/ntshengn_networking_common.h
+++ src/networking/ntshengn_networking_common.h
@@ -1,5 +1,40 @@
 #pragma once
-#include "../utils/ntshengn_core_defines.h"
+#include <cstdint>
+
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_NETWORKING_INFO(message) \
+	do { \
+		std::cerr << "\33[1m\33[93mNETWORKING \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
+	} while(0)
+#else
+#define NTSHENGN_NETWORKING_INFO(message) \
+	do { \
+	} while(0)
+#endif
+
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_NETWORKING_WARNING(message) \
+	do { \
+		std::cerr << "\33[1m\33[93mNETWORKING \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
+	} while(0)
+#else
+#define NTSHENGN_NETWORKING_WARNING(message) \
+	do { \
+	} while(0)
+#endif
+
+#if defined(NTSHENGN_DEBUG)
+#define NTSHENGN_NETWORKING_ERROR(message, code) \
+	do { \
+		std::cerr << "\33[1m\33[93mNETWORKING \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
+		exit(1); \
+	} while(0)
+#else
+#define NTSHENGN_NETWORKING_ERROR(message, code) \
+	do { \
+		exit(1); \
+	} while(0)
+#endif
 
 namespace NtshEngn {
 
