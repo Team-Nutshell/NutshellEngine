61b341114106c62b62c727cc1b3dc7850647c1e9
ntsh-oni <teamnutshell.dev@gmail.com>
2023-09-27 17:08:18 +0200

    add constness to frame limiter wait variables

--- frame_limiter/ntshengn_frame_limiter.cpp
+++ frame_limiter/ntshengn_frame_limiter.cpp
@@ -3,10 +3,10 @@
 
 void NtshEngn::FrameLimiter::wait(double frameStart) {
 	if (m_maxFPS != 0) {
-		double currentTime = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
-		double maxFPSToMilliseconds = 1000.0 / static_cast<double>(m_maxFPS);
+		const double currentTime = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
+		const double maxFPSToMilliseconds = 1000.0 / static_cast<double>(m_maxFPS);
 		if ((currentTime - frameStart) < maxFPSToMilliseconds) {
-			double busyWaitingStart = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
+			const double busyWaitingStart = std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count();
 			while ((std::chrono::duration<double, std::milli>(std::chrono::steady_clock::now().time_since_epoch()).count() - busyWaitingStart) < (maxFPSToMilliseconds - (currentTime - frameStart)));
 		}
 	}
