06b359e29738dfd5279b0dd264ad306baa19bfb3
ntsh-oni <teamnutshell.dev@gmail.com>
2024-05-04 12:07:03 +0200

    don't open console if the build type is Release on Windows

--- main.cpp
+++ main.cpp
@@ -1,3 +1,7 @@
+#if defined(NTSHENGN_OS_WINDOWS) && defined(NTSHENGN_RELEASE)
+#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
+#endif
+
 #include "ntshengn_core.h"
 
 int main() {
