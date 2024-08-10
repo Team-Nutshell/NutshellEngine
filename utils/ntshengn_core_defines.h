1365a4d88d27c65c91e2a98ebfe4e29f22bc2b40
ntsh-oni <teamnutshell.dev@gmail.com>
2024-08-10 20:14:51 +0200

    replace ostreams for strings to avoid unwanted flushes

--- utils/ntshengn_core_defines.h
+++ utils/ntshengn_core_defines.h
@@ -4,7 +4,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_INFO(message) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " << message << std::endl; \
+		std::cerr << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
 #else
 #define NTSHENGN_CORE_INFO(message) \
@@ -15,7 +15,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_WARNING(message) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " << message << std::endl; \
+		std::cerr << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
 #else
 #define NTSHENGN_CORE_WARNING(message) \
@@ -26,7 +26,7 @@
 #if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_ERROR(message, code) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " << message << " (" << #code << ")" << std::endl; \
+		std::cerr << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " + std::string(message) + " (" + std::string(#code) + ")" << std::endl; \
 		exit(1); \
 	} while(0)
 #else
