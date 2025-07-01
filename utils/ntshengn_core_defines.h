6011bfd10f070181baeea4a0ebd957989271a208
ntsh-oni <teamnutshell.dev@gmail.com>
2025-07-01 07:21:03 +0200

    log in Release

--- utils/ntshengn_core_defines.h
+++ utils/ntshengn_core_defines.h
@@ -1,37 +1,18 @@
 #pragma once
 #include <iostream>
 
-#if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_INFO(message) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " + std::string(message) << std::endl; \
+		std::cout << "\33[1m\33[39mCORE \33[34mINFO\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
-#else
-#define NTSHENGN_CORE_INFO(message) \
-	do { \
-	} while(0)
-#endif
 
-#if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_WARNING(message) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " + std::string(message) << std::endl; \
+		std::cout << "\33[1m\33[39mCORE \33[93mWARNING\33[39m\33[0m: " + std::string(message) << std::endl; \
 	} while(0)
-#else
-#define NTSHENGN_CORE_WARNING(message) \
-	do { \
-	} while(0)
-#endif
 
-#if defined(NTSHENGN_DEBUG)
 #define NTSHENGN_CORE_ERROR(message) \
 	do { \
-		std::cerr << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " + std::string(message) << std::endl; \
+		std::cout << "\33[1m\33[39mCORE \33[31mERROR\33[39m\33[0m: " + std::string(message) << std::endl; \
 		exit(1); \
 	} while(0)
-#else
-#define NTSHENGN_CORE_ERROR(message) \
-	do { \
-		exit(1); \
-	} while(0)
-#endif
